5b99655b736bc918c3f8a445846c36409abe00d7
uk2459644 <uk2459644@gmail.com>
2020-12-25 14:17:27 +0530

    First page top optimized for seo

--- pages/category/mockcat.js
+++ pages/category/mockcat.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import FirstPageTop from "../../components/CustomPages/FirstPageTop";
 import MockTest from "../../components/CustomPages/MockTest";
 import FooterBlack1 from "../../components/Footer/FooterBlack1";
@@ -7,6 +8,18 @@ export default function MockCategoy(){
 
     return(
         <div>
+              <Head>
+        <title>MOCK-TESTS: Online tests to enrich your confidence.</title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <meta name="description" content="It's always a great idea to boost your confidence, before exam.Choose a category and do as many tests as you can . "/>
+        <meta name="robots" content="index, follow" />
+        <meta property="og:title" content="MOCK-TESTS: Online tests to enrich your confidence" />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content="It's always a great idea to boost your confidence, before exam.Choose a category and do as many tests as you can . "/>
+        <meta property="og:image" content="https://mocktest.vercel.app/pexels-kulik-stepan-2.jpg" /> 
+        <link rel="icon" href="https://mocktest.vercel.app/category/mockcat" />
+      </Head>
             <HomeNav />
             <FirstPageTop />
             <MockTest />
