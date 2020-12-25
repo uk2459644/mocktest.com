82f41bc3de6cda705f601a4bf706681e1874c72e
uk2459644 <uk2459644@gmail.com>
2020-12-25 07:42:34 +0530

    first page and about us page seo optimized

--- pages/aboutus/About.js
+++ pages/aboutus/About.js
@@ -1,4 +1,6 @@
+import Head from "next/head";
 import AboutUs from "../../components/CustomPages/AboutUs";
+import FooterBlack1 from "../../components/Footer/FooterBlack1";
 import HomeNav from "../../components/NavBar/HomeNavBar";
 
 
@@ -6,8 +8,35 @@ export default function About(){
 
     return(
         <div>
+             <Head>
+        <title>ABOUT US : WE BELIEVE IN “PEOPLE GOALS” YOURS AND OURS.</title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <meta name="description" content="
+       Are you passionate about “Your Goals ?”.
+
+       Have you a dream of “Govt Jobs ?”
+       
+       Why are you waiting for ?
+       
+       Join our awesome community of “Govt Jobs aspirants”and begin your journey in a more confident way ."/>
+        <meta name="robots" content="index, follow" />
+        <meta property="og:title" content="ABOUT US : WE BELIEVE IN “PEOPLE GOALS” YOURS AND OURS." />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content="Are you passionate about “Your Goals ?”.
+
+Have you a dream of “Govt Jobs ?”
+
+Why are you waiting for ?
+
+Join our awesome community of “Govt Jobs aspirants”and begin your journey in a more confident way . "/>
+        <meta property="og:image" content="https://mocktest.vercel.app/pexels-kulik-stepan-2.jpg" /> 
+        <link rel="icon" href="https://mocktest.vercel.app/aboutus/About" />
+      </Head>
+
             <HomeNav />
  <AboutUs />
+ <FooterBlack1 />
         </div>
        
     )
--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,21 @@ import HomeNav from '../components/NavBar/HomeNavBar'
 export default function Home() {
   return (
     <div>
+       <Head>
+        <title>Mock-Test : GET PREPARED, CONFIDENT & MOTIVATED</title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <meta name="description" content="
+        An exercise system that keeps you safe, makes things simple, and gives you support so you feel capable and confident.
+                                             "/>
+        <meta name="robots" content="index, follow" />
+        <meta property="og:title" content="Mock-Test : GET PREPARED, CONFIDENT & MOTIVATED " />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content="An exercise system that keeps you safe, makes things simple, and gives you support so you feel capable and confident.
+                                             " />
+        <meta property="og:image" content="https://mocktest.vercel.app/study-1.jpg" /> 
+        <link rel="icon" href="https://mocktest.vercel.app/" />
+      </Head>
 
       <HomeNav />
 
