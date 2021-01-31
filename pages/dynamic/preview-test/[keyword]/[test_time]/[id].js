9ba1f1d044af45460e62378b558d18e2ba08af5f
uk2459644 <uk2459644@gmail.com>
2021-01-31 19:28:00 +0530

    indication for correct answer and user answer added

--- pages/dynamic/preview-test/[keyword]/[test_time]/[id].js
+++ pages/dynamic/preview-test/[keyword]/[test_time]/[id].js
@@ -86,7 +86,7 @@ export default function QuestionPageRRBGROUPD({ questions, test_time }) {
   const [qset, setQset] = useState(questions.questions)
   const [qlength, setQlength] = useState(questions.questions.length - 1)
   const [questionNumber, setQuestionNumber] = useState(0)
-  const [time_limit,setTimeLimit]=useState(test_time);
+  const [time_limit, setTimeLimit] = useState(test_time);
   // const [testname,setTestname]=useState(keyword);
   // const [useranswer,setUserAnswer]=useState(new Array(questions.questions.length).fill(''));
 
@@ -357,11 +357,11 @@ export default function QuestionPageRRBGROUPD({ questions, test_time }) {
     // alert(`some thing clicked  ${useranswer[questionNumber].user_ans}`);
   }
 
- 
+
 
   return (
     <div id="testpage" className="bg-white opacity-100">
-      
+
 
       {
         qset.length > 0 &&
@@ -369,7 +369,7 @@ export default function QuestionPageRRBGROUPD({ questions, test_time }) {
           qset.length &&
           answerdata.length > 0 ? (
             <div className={` ${ontest && analysis_bool ? 'block' : 'hidden'} `}>
-              
+
 
               <PracticeQuestionCard
 
@@ -433,9 +433,33 @@ export default function QuestionPageRRBGROUPD({ questions, test_time }) {
           />
         </div>
         <AnalysisFooter
-          link="/rrb-group-d/rrb-group-d-index"
+          link="/dynamic/dynamic-test-cate-index"
         />
         <div>
+          <div className="flex flex-row flex-wrap justify-center content-center align-center items-center">
+           
+            <div className="flex flex-row inline-flex items-center justify-center m-4 px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50 ">
+              <span className="h-6 flex items-center sm:h-7 p-2 ">
+                <svg className="flex-shrink-0 h-5 w-5 text-cyan-500" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+                </svg>
+              </span>
+              <p>
+                Correct Answer
+              </p>
+            </div>
+            <div className="flex flex-row inline-flex items-center justify-center m-4 px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
+              <span className="h-6 flex items-center sm:h-7 p-2 ">
+                <svg className="flex-shrink-0 h-5 w-5 text-green-500" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+                </svg>
+              </span>
+              <p>
+                Your Answer
+              </p>
+            </div>
+
+          </div>
           {
             analysis_answer_data !== null ? (
               <div>
